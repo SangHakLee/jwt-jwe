d9f56b9247ced21368dc48fd8dd943758c3817f0
SangHakLee <leesanghak7298@gmail.com>
2017-06-15 00:46:26 +0900

    오류 기존 jwt  와  jwe 어떻게 처리할지 더 생각

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken'
+class Jwt {
+  // constructor (payload, secret, option) {
+  constructor () {
+  }
+
+  get token () {
+
+  }
+
+  signE (payload, secret, option) {
+
+  }
+}
+
+// NOTE extends not allow. jwt is object
+Object.assign(Jwt, jwt) 
+
+export default Jwt
--- test/jwt-jwe.spec.js
+++ test/jwt-jwe.spec.js
@@ -1,7 +1,14 @@
+'use strict'
+const debug = require('debug')
+
+const Jwt = require('../lib').default
+console.log('Jwt', Jwt)
+
 describe(`jwt-jwe`, () => {
   describe(`test`, () => {
     it(`1`, () => {
-      
+      const jwt = new Jwt()
+      debug(jwt)
     })
   })
 })
